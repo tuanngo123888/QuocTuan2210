b95b89d67554f15beef5115b6f22de30ae9282ec
tuanngo123888 <ngoquoctuannn@gmail.com>
2025-08-01 08:33:45 +0700

    Create de2.js

---
+++ de2.js
@@ -0,0 +1,74 @@
+const transactions = [
+    {
+        id: 1102,
+        khachHang: "Võ Hoài An",
+        nhanVien: "Mai Thục Anh",
+        soTien: 250000,
+        ngayMua: "2024-06-06T09:00:00"
+    },
+    {
+        id: 1199,
+        khachHang: "Hoàng Thị Thắng",
+        nhanVien: "Nguyễn Văn Hồng",
+        soTien: 600000,
+        ngayMua: "2024-06-06T09:03:00"
+    },
+    {
+        id: 1299,
+        khachHang: "Nguyễn Huy Quang",
+        nhanVien: "Nguyễn Văn Hồng",
+        soTien: 924000,
+        ngayMua: "2024-06-06T09:10:00"
+    },
+    {
+        id: 1677,
+        khachHang: "Huỳnh Văn Nam",
+        nhanVien: "Mai Thục Anh",
+        soTien: 150000,
+        ngayMua: "2024-06-06T09:20:00"
+    },
+    {
+        id: 1439,
+        khachHang: "Nguyễn Hồng Minh",
+        nhanVien: "Mai Thục Anh",
+        soTien: 354000,
+        ngayMua: "2024-06-06T09:24:00"
+    },
+
+    {
+        id: 1882,
+        khachHang: "Trần Văn Long",
+        nhanVien: "Nguyễn Văn Hồng",
+        soTien: 1250000,
+        ngayMua: "2024-06-07T10:05:00"
+    },
+    {
+        id: 1891,
+        khachHang: "Lê Thị Hoa",
+        nhanVien: "Mai Thục Anh",
+        soTien: 78000,
+        ngayMua: "2024-06-07T11:30:00"
+    },
+    {
+        id: 1904,
+        khachHang: "Phạm Minh Tuấn",
+        nhanVien: "Trần Thị Bích",
+        soTien: 430000,
+        ngayMua: "2024-06-08T14:00:00"
+    },
+    {
+        id: 1911,
+        khachHang: "Bùi Thu Trang",
+        nhanVien: "Nguyễn Văn Hồng",
+        soTien: 89000,
+        ngayMua: "2024-06-08T15:12:00"
+    },
+    {
+        id: 1925,
+        khachHang: "Đỗ Gia Huy",
+        nhanVien: "Mai Thục Anh",
+        soTien: 2100000,
+        ngayMua: "2024-06-09T08:45:00"
+    }
+
+];
