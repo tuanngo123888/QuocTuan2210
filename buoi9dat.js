fdd893eafa885a38ea48eaf8dd8af2a711950c6e
tuanngo123888 <ngoquoctuannn@gmail.com>
2025-08-04 09:09:47 +0700

    Create buoi9dat.js

---
+++ buoi9dat.js
@@ -0,0 +1,30 @@
+const ordersData = [
+  {
+    id: 1,
+    tenKhach: "Mai",
+    hoDem: "Thục Anh",
+    diaChi: "23, Hoàng Đạo Thúy, Thanh Xuân, Hà Nội",
+    hoatDong: true
+  },
+  {
+    id: 2,
+    tenKhach: "Hoàng",
+    hoDem: "Sinh Hồng",
+    diaChi: "23, Hoàng Đạo Thúy, Thanh Xuân, Hà Nội",
+    hoatDong: false
+  },
+  {
+    id: 3,
+    tenKhach: "Tính",
+    hoDem: "Mai Trung",
+    diaChi: "1411, Đường Láng, Cầu Giấy, Hà Nội",
+    hoatDong: true
+  },
+  {
+    id: 4,
+    tenKhach: "Hồng",
+    hoDem: "Nguyễn Văn",
+    diaChi: "1411, Đường Láng, Cầu Giấy, Hà Nội",
+    hoatDong: false
+  }
+];
